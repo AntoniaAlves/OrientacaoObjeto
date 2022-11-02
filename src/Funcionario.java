f44b7fe6ffc4d7adb3b6c89545f93c55d12b86d7
Antonia Alves <dhany-ele@hotmail.com>
2022-11-02 09:41:25 -0300

    Aula Orientacao Objeto

---
+++ src/Funcionario.java
@@ -0,0 +1,17 @@
+
+public class Funcionario {
+	
+	public String nome;
+	public double valorPorHora;
+	public int horas;
+	
+	
+		
+
+
+	public double total() {
+		// TODO Auto-generated method stub
+		return  valorPorHora * horas;
+	}
+
+}
---
+++ src/Program.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+	public static void  main(String[]args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in); 
+		
+		Funcionario f1, f2;
+		
+		f1 = new Funcionario();
+		f2 = new Funcionario();
+	
+	
+	
+	System.out.println("Dados do primeiro funcionario: ");
+	f1.nome = sc.next();
+	f1.valorPorHora = sc.nextDouble();
+	f1.horas = sc.nextInt();
+	
+	System.out.println("Dados do segundo funcionario: ");
+	f2.nome = sc.next();
+	f2.valorPorHora = sc.nextDouble();
+	f2.horas = sc.nextInt();
+	
+	 double total = f1.total() + f2.total();
+	 
+	 System.out.printf("Total = %.2f%n", total);
+
+}
+	
+}
